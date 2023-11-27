a00d7407e3137f8cbca577c6af8104c2ce07e2e9
Aryan Walia <aryan.3walia@gmail.com>
2023-11-27 23:50:45 +0530

    Go

---
+++ Tweetbook/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Configuration;
+using Tweetbook.Options;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen(_ =>
+{
+    _.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Tweetbook API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+var swaggerOptions = new SwaggerOptions();
+builder.Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+app.UseSwagger(option => 
+{
+    option.RouteTemplate = swaggerOptions.JsonRoute;
+});
+
+app.UseSwaggerUI(option =>
+{
+    option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
